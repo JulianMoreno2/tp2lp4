82e9e0cebd0618c46c1431ba6dfdeeb76652febe
Rain <julianezequiemoreno@gmail.com>
2016-03-27 12:19:31 -0300

    borro metodo onCreateOptionsMenu del main activity

--- MemoTest/app/src/main/java/com/example/ddavi/memotest/MainActivity.java
+++ MemoTest/app/src/main/java/com/example/ddavi/memotest/MainActivity.java
@@ -62,14 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     @Override
     public void onBackPressed() {
         //Deshabilito boton que me permite ir hacia atrás
